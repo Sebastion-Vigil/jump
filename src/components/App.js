7ae46459392e1cc9844a53eae75bee522c21232d
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-10-11 01:10:30 -0500

    closer, one method for jump press down, another method for jump press released

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,34 @@ import React from 'react'
 import '../css/App.css'
 
 class App extends React.Component {
-    state = {
-        squareTop: '423px'
-    }
-    handleJump = () => {
-        // more stuff soon
-    }
-    render() {
-        return (
-            <div className='App'>
-                <div className='square-section'>
-                    <div
-                      className='square'
-                      style={{
-                          top: this.state.squareTop
-                      }}
-                    ></div>
-                </div>
-                <div className='jump-button'></div>
-            </div>
-        )
-    }
+  state = {
+    squareTop: '423px'
+  }
+  handleJumpStart = () => {
+    console.log('jump started!')
+  }
+  handleJumpEnd = () => {
+    console.log('jump ended!')
+  }
+  render () {
+    return (
+      <div className='App'>
+        <div className='square-section'>
+          <div
+            className='square'
+            style={{
+              top: this.state.squareTop
+            }}
+          ></div>
+        </div>
+        <div 
+          className='jump-button'
+          onMouseDown={this.handleJumpStart}
+          onMouseUp={this.handleJumpEnd}
+        ></div>
+      </div>
+    )
+  }
 }
 
 export default App
