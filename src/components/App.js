d54a9d9947ebba092284307e9afef319c2634995
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-10-10 22:24:04 -0500

    basic structure in place, now to implement jump logic

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,22 @@ import React from 'react'
 import '../css/App.css'
 
 class App extends React.Component {
+    state = {
+        squareTop: '410px'
+    }
     render() {
         return (
-            <div className='App'>Jump!</div>
+            <div className='App'>
+                <div className='square-section'>
+                    <div
+                      className='square'
+                      style={{
+                          top: this.state.squareTop
+                      }}
+                    ></div>
+                </div>
+                <div className='jump-button'></div>
+            </div>
         )
     }
 }
