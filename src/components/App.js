8c3cbfebecdd1185eaac6f8fe618a5dd324ecf0d
link <irishseanvaldivia@gmail.com>
2020-10-30 17:35:12 -0500

    plan set, empty methods in place, square will move as button is being pressed for more responsiveness to game

--- src/components/App.js
+++ src/components/App.js
@@ -8,93 +8,32 @@ class App extends React.Component {
   state = {
     squareTop: '423px',
     jumping: false,
-    buttonTimer: undefined,
-    upTimer: undefined,
-    downTimer: undefined,
-    upSpeed: 1,
-    downSpeed: 1,
-    buttonPressDuration: undefined,
-    targetHeight: 100,
-    currentHeight: 0
+    buttonPressDuration: undefined, 
+    targetHT: 100,
+    currentHT: 0
   }
-  handleButtonDown = () => {
-    if (this.state.jumping) return // exit function if already in jumping state
-    this.setState({
-      buttonTimer: setInterval(() => {
-        let pressDuration = !isNaN(this.state.buttonPressDuration)
-          ? this.state.buttonPressDuration
-          : 0
-        let targetHT = this.state.targetHeight
-        if (pressDuration >= 50 && targetHT >= 300) {
-          this.handleButtonDuration()
-          return
-        }
-        pressDuration += 1
-        targetHT = pressDuration * 6 > 100 ? pressDuration * 6 : targetHT
-        this.setState({
-          buttonPressDuration: pressDuration,
-          targetHeight: targetHT
-        })
-      }, 1)
-    })
+
+  handleButtonPress = () => {
+    // this method handles it all
+    // is already jumping?
+    // need to ascend?
+    // need to pause?
+    // need to descend?
+    // need to reset?
   }
-  handleButtonDuration = () => {
-    if (this.state.jumping || isNaN(this.state.buttonPressDuration)) return
-    clearInterval(this.state.buttonTimer)
-    this.handleJumpPhysics()
-  }
-  handleJumpPhysics = () => {
-    this.ascend()
-  }
+
   ascend = () => {
-    let speed = this.state.upSpeed
-    this.setState({
-      jumping: true,
-      upTimer: setInterval(() => {
-        let squareT = parseInt(this.state.squareTop)
-        let currentHT = this.state.currentHeight
-        if (currentHT >= this.state.targetHeight) {
-          this.descend()
-          return
-        }
-        squareT -= 3
-        currentHT += 3
-        this.setState({
-          squareTop: squareT + 'px',
-          currentHeight: currentHT
-        })
-      }, speed)
-    })
   }
+
+  pause = () => {
+  }
+
   descend = () => {
-    clearInterval(this.state.upTimer)
-    this.setState({
-      downTimer: setInterval(() => {
-        let squareT = parseInt(this.state.squareTop)
-        let currentHT = this.state.currentHeight
-        if (currentHT <= 0) {
-          this.handleJumpFinished()
-          return
-        }
-        squareT += 3
-        currentHT -= 3
-        this.setState({
-          squareTop: squareT + 'px',
-          currentHeight: currentHT
-        })
-      }, this.state.downSpeed)
-    })
   }
-  handleJumpFinished = () => {
-    clearInterval(this.state.downTimer)
-    console.log('targetHT jump finished: ', this.state.targetHeight)
-    this.setState({
-      buttonPressDuration: undefined,
-      targetHeight: 100,
-      currentHeight: 0,
-      jumping: false
-    })
+
+  reset = () => {
   }
+  
   render () {
     return (
       <div className='App'>
@@ -108,8 +47,7 @@ class App extends React.Component {
         </div>
         <div
           className='jump-button'
-          onMouseDown={this.handleButtonDown}
-          onMouseUp={this.handleButtonDuration}
+          onMouseDown={this.handleButtonPress}
         ></div>
       </div>
     )
@@ -127,7 +65,11 @@ export default App
 // do we really need a onMouseUp() method?
   // (methinks not)
 // handleButtonPress()
-  //
+  // is already jumping?
+  // need to ascend?
+  // need to descend?
+  // need to pause?
+  // time to reset?
 // ascend()
   //
 // descend()
