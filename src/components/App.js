d4a0ea26b16d0c667063ba27b50ca6e1980dc1fd
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2020-12-02 19:08:03 -0600

    square jumps up and down, no bugs, now to implement slight pause after ascension and before descension

--- src/components/App.js
+++ src/components/App.js
@@ -7,48 +7,65 @@ import '../css/App.css'
 class App extends React.Component {
   state = {
     squareTop: '423px',
-    jumping: false,
+    goingUp: false,
+    goingDown: false,
     buttonPressDuration: 0,
     buttonTimer: undefined,
     descendTimer: undefined
   }
 
-  handleButtonDown = () => {
-    if (this.state.jumping) return
-    let pressDuration = 0
+  startAscension = () => {
+    console.log('starting ascension!')
     this.setState({
-      jumping: true,
+      goingUp: true,
+    })
+  }
+
+  startDescension = () => {
+    console.log('starting descension!')
+    this.setState({
+      goingDown: true
+    })
+  }
+
+  handleButtonDown = () => {
+    if (this.state.goingUp) return
+    this.startAscension()
+    let pressDuration = this.state.buttonPressDuration
+    this.setState({
       buttonTimer: setInterval(() => {
-        if (this.state.buttonPressDuration > 125) {
+        console.log('pressDuration: ', pressDuration)
+        if (pressDuration > 125) {
           this.handleButtonUp()
           return
         }
-        console.log(this.state.buttonPressDuration)
         this.ascend()
         pressDuration += 1
         this.setState({
           buttonPressDuration: pressDuration
         })
-      }, 1)
+      })
     })
   }
 
   handleButtonUp = () => {
+    if (this.state.goingDown) return
+    if (parseInt(this.state.squareTop) >= 423) return
+    this.startDescension()
     clearInterval(this.state.buttonTimer)
     this.setState({
       descendTimer: setInterval(() => {
+        this.descend()
         if (parseInt(this.state.squareTop) >= 423) {
           this.reset()
           return
         }
-        this.descend()
       })
     })
   }
 
   ascend = () => {
     console.log('going up!')
-    // this should only ascend the square
     let squareT = parseInt(this.state.squareTop)
     squareT -= 3
     this.setState({
@@ -58,7 +75,6 @@ class App extends React.Component {
 
   descend = () => {
     console.log('going down!')
-    // this should only descend the square
     let squareT = parseInt(this.state.squareTop)
     squareT += 3
     this.setState({
@@ -69,7 +85,9 @@ class App extends React.Component {
   reset = () => {
     clearInterval(this.state.descendTimer)
     this.setState({
-      jumping: false,
+      squareTop: '423px',
+      goingUp: false,
+      goingDown: false,
       buttonPressDuration: 0
     })
   }
@@ -88,6 +106,7 @@ class App extends React.Component {
         <div
           className='jump-button'
           onMouseDown={this.handleButtonDown}
+          onMouseUp={this.handleButtonUp}
         ></div>
       </div>
     )
